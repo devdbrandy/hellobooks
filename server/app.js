b357c087c03a63307ddc1d6f5b93d21db33109fc
Dbrandy <devdbrandy@gmail.com>
2017-09-03 12:47:51 +0100

    Remove globals for error rendering

--- server/app.js
+++ server/app.js
@@ -23,11 +23,7 @@ App.use((req, res, next) => {
 
 // error handler
 App.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  // render the error
   res.status(err.status || 500);
   res.json({
     status: err.status,
